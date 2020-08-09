ae87667915b97ae89e64fdd9e709d0190290aa8c
Liam Wallace <45268305+Lwallace3@users.noreply.github.com>
2020-08-10 00:11:52 +0100

    Made to run using selenium to watch on website

--- py2048_easy.py
+++ py2048_easy.py
@@ -1,14 +1,14 @@
-"""
-Python 2048 Game : Basic Console Based User Interface For Game Play
-
-Originally written by Phil Rodgers, University of Strathclyde
-"""
-
-from py2048_classes import Board, Tile
-
-import time
+import re
 import math
-import random
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+
+from py2048_classes import Tile, Board
+
 
 def pickMove(board):
     move = None
@@ -22,32 +22,34 @@ def pickMove(board):
 
         if x[0] > topScore:
             topScore = x[0]
-            move = x[1] 
+            move = x[1]
     return move
-            
+
 
 def expectimax(board, depth, player, move):
     if depth == 0:
         return utilityScore(board), move
     elif player == 1:
-        return maxVal(board,depth,player,move)
+        return maxVal(board, depth, player, move)
     elif player == 2:
-        return expVal(board, depth,player,move)
-    
-def maxVal(board,depth,player,move):
+        return expVal(board, depth, player, move)
+
+
+def maxVal(board, depth, player, move):
     score = 0
     for i in board.possible_moves():
         x = board.export_state()
         tempBoard = Board(x)
         tempBoard.make_move(i)
 
-        currentScore = expectimax(tempBoard, depth-1, 2, i)
+        currentScore = expectimax(tempBoard, depth - 1, 2, i)
         if currentScore[0] > score:
             score = currentScore[0]
             move = currentScore[1]
 
     return score, move
-        
+
+
 def expVal(board, depth, player, move):
     totalScore = 0
     emptyCells = board.empty()
@@ -57,17 +59,18 @@ def expVal(board, depth, player, move):
         tempBoard = Board(x)
         fourTile = Tile(2)
         tempBoard.grid[i[1]][i[0]] = fourTile
-        currentScore = expectimax(tempBoard, depth-1, 1, move)
+        currentScore = expectimax(tempBoard, depth - 1, 1, move)
         totalScore += (0.2 * currentScore[0])
 
         tempBoard = Board(x)
         twoTile = Tile(1)
         tempBoard.grid[i[1]][i[0]] = twoTile
-        currentScore = expectimax(tempBoard, depth-1, 1, move)
+        currentScore = expectimax(tempBoard, depth - 1, 1, move)
         totalScore += (0.8 * currentScore[0])
-            
-    average = totalScore/len(emptyCells)
+
+    average = totalScore / len(emptyCells)
     return average, move
+
 
 def utilityScore(board):
     utility = 0
@@ -76,97 +79,89 @@ def utilityScore(board):
     bottomLeftVal = 0
     bottomRightVal = 0
     boardGrid = board.export_state()
-    
-    
+
     comparisonGridLeft = [
-                [2**6, 2**5, 2**4, 2**3],
-                [2**5, 2**4, 2**3, 2**2],
-                [2**4, 2**3, 2**2, 2**1],
-                [2**3, 2**2, 2**1, 2**1]
-            ]
+        [2 ** 6, 2 ** 5, 2 ** 4, 2 ** 3],
+        [2 ** 5, 2 ** 4, 2 ** 3, 2 ** 2],
+        [2 ** 4, 2 ** 3, 2 ** 2, 2 ** 1],
+        [2 ** 3, 2 ** 2, 2 ** 1, 2 ** 1]
+    ]
     comparisonGridRight = [
-                [2**3, 2**4, 2**5, 2**6],
-                [2**2, 2**3, 2**4, 2**5],
-                [2**1, 2**2, 2**3, 2**4],
-                [2**1, 2**1, 2**2, 2**3]
-            ]
+        [2 ** 3, 2 ** 4, 2 ** 5, 2 ** 6],
+        [2 ** 2, 2 ** 3, 2 ** 4, 2 ** 5],
+        [2 ** 1, 2 ** 2, 2 ** 3, 2 ** 4],
+        [2 ** 1, 2 ** 1, 2 ** 2, 2 ** 3]
+    ]
     comparisonGridBottomRight = [
-                [2**1, 2**1, 2**2, 2**3],
-                [2**1, 2**2, 2**3, 2**4],
-                [2**2, 2**3, 2**4, 2**5],
-                [2**3, 2**4, 2**5, 2**6]
-            ]
+        [2 ** 1, 2 ** 1, 2 ** 2, 2 ** 3],
+        [2 ** 1, 2 ** 2, 2 ** 3, 2 ** 4],
+        [2 ** 2, 2 ** 3, 2 ** 4, 2 ** 5],
+        [2 ** 3, 2 ** 4, 2 ** 5, 2 ** 6]
+    ]
     comparisonGridBottomLeft = [
-                [2**3, 2**2, 2**1, 2**1],
-                [2**4, 2**3, 2**2, 2**1],
-                [2**5, 2**4, 2**3, 2**2],
-                [2**6, 2**5, 2**4, 2**3]
-            ]
-    
-    for x in range(0,4):
-        for y in range(0,4):
+        [2 ** 3, 2 ** 2, 2 ** 1, 2 ** 1],
+        [2 ** 4, 2 ** 3, 2 ** 2, 2 ** 1],
+        [2 ** 5, 2 ** 4, 2 ** 3, 2 ** 2],
+        [2 ** 6, 2 ** 5, 2 ** 4, 2 ** 3]
+    ]
+
+    for x in range(0, 4):
+        for y in range(0, 4):
             if boardGrid[x][y] != None:
-                val = board.grid[x][y].get_tile_value() 
+                val = board.grid[x][y].get_tile_value()
                 leftVal += val * comparisonGridLeft[x][y]
                 rightVal += val * comparisonGridRight[x][y]
                 bottomRightVal += val * comparisonGridBottomRight[x][y]
                 bottomLeftVal += val * comparisonGridBottomLeft[x][y]
-           
+
     utility = max(rightVal, max(leftVal, max(bottomLeftVal, bottomRightVal)))
     return utility
 
-    
+
+def update_grid(driver):
+    grid = [
+        [None, None, None, None],
+        [None, None, None, None],
+        [None, None, None, None],
+        [None, None, None, None]
+    ]
+
+    tc = driver.find_element_by_class_name('tile-container')
+    tile_html = tc.get_attribute('innerHTML')
+    find_values = re.findall(r'tile-\b\d{1,8}\b', tile_html)
+    find_positions = re.findall(r'tile-position-\d-\d', tile_html)
+
+    for x in range(len(find_values)):
+        x_coord = int(find_positions[x][-3]) - 1
+        y_coord = int(find_positions[x][-1]) - 1
+        real_value_of_tile = int(find_values[x].replace('tile-', ''))
+        log_value_of_tile = math.log(real_value_of_tile, 2)
+        grid[y_coord][x_coord] = Tile(log_value_of_tile)
+    return grid
+
+
 def main():
-#    allmoves = ['UP','LEFT','DOWN','RIGHT']
+    driver = webdriver.Chrome(executable_path='C:/Program Files (x86)/chromedriver.exe')
+    driver.get('https://play2048.co/')
+    body = driver.find_element_by_tag_name('body')
     board = Board()
-    board.add_random_tiles(2)
-    print("main code")
+    key_map = {
+        'UP': Keys.ARROW_UP,
+        'RIGHT': Keys.ARROW_RIGHT,
+        'DOWN': Keys.ARROW_DOWN,
+        'LEFT': Keys.ARROW_LEFT
+    }
+    board.grid = update_grid(driver=driver)
 
-    move_counter = 0
-    move = None
-    move_result = False
-
-    
-    overalltime=time.time()
     while True:
-        print("Number of successful moves:{}, Last move attempted:{}:, Move status:{}".format(move_counter, move, move_result))
-        print(board)
-       # print(board.print_metrics())
-        if board.possible_moves()==[]:
-            if (board.get_max_tile()[0]<2048):
-                print("You lost!")
-            else:
-                print("Congratulations - you won!")
-            break
         begin = time.time()
-###################################### Your code should be inserted below 
-###################################### (feel free to define additional functions to determine the next move)
-   
         move = pickMove(board)
-        print(move)
-        board.make_move(move)
+        next_key = key_map[move]
+        body.send_keys(next_key)
 
-######################################  Do not   4 lines below      
-######################################
-        print("Move time: ", time.time()-begin)
-        board.add_random_tiles(1)
-        move_counter = move_counter + 1
-    print("Average time per move:", (time.time()-overalltime)/move_counter)
-    
+        print(move, " at a time of ", (time.time() - begin))
+        board.grid = update_grid(driver=driver)
+
 
 if __name__ == "__main__":
     main()
-    
-    
-def monteCarlo(board):
-    maxGame = board.random_rollout(100)
-    i = 0
-    for i in range(100):
-        boardCopy = board
-        
-        runGame = boardCopy.random_rolloutB(100)
-        if(runGame[0] > maxGame[0]):
-            maxGame = runGame
-    return maxGame[1]
-
-
